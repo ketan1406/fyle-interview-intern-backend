5ebb1623cc1601d8272090ae8074ed1b647c8c6c
Ketan Saini <sainiketan99@gmail.com>
2024-10-01 11:22:48 +0530

    Modified function to default the status code of the response to 200

--- core/apis/responses.py
+++ core/apis/responses.py
@@ -1,7 +1,7 @@
 from flask import Response, jsonify, make_response
 
-
 class APIResponse(Response):
     @classmethod
-    def respond(cls, data):
-        return make_response(jsonify(data=data))
+    def respond(cls, data, status_code=200):
+        # Added the status code parameter to default to 200
+        return make_response(jsonify(data=data), status_code)
