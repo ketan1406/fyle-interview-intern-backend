d97ec0301c0b50a4d0a0b6f5dbac41a00098fa02
Ketan Saini <sainiketan99@gmail.com>
2024-10-01 13:40:47 +0530

    Added Validations to grading endpoint

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
+from core.models.assignments import Assignment, AssignmentStateEnum
 
 from .schema import AssignmentSchema, AssignmentGradeSchema
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
@@ -12,7 +12,7 @@ teacher_assignments_resources = Blueprint('teacher_assignments_resources', __nam
 @decorators.authenticate_principal
 def list_assignments(p):
     """Returns list of assignments"""
-    teachers_assignments = Assignment.get_assignments_by_teacher()
+    teachers_assignments = Assignment.get_assignments_by_teacher(p.teacher_id)
     teachers_assignments_dump = AssignmentSchema().dump(teachers_assignments, many=True)
     return APIResponse.respond(data=teachers_assignments_dump)
 
@@ -23,6 +23,20 @@ def list_assignments(p):
 def grade_assignment(p, incoming_payload):
     """Grade an assignment"""
     grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    assignment = Assignment.get_by_id(grade_assignment_payload.id)
+
+    # Check if the assignment exists
+    if assignment is None:
+        return APIResponse.respond_error('FyleError', 'Assignment not found', 404)
+
+    # Check if the teacher is the correct one for this assignment
+    if assignment.teacher_id != p.teacher_id:
+        return APIResponse.respond_error('FyleError', 'You are not authorized to grade this assignment.', 400)
+    
+    # Ensure the assignment is in a valid state to be graded
+    if assignment.state == AssignmentStateEnum.DRAFT:
+        return APIResponse.respond_error('FyleError', 'Only submitted assignments can be graded.', 400)
 
     graded_assignment = Assignment.mark_grade(
         _id=grade_assignment_payload.id,
