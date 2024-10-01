00160d53f0856ea8f02108b02d2b91b3dbd0affd
Ketan Saini <sainiketan99@gmail.com>
2024-10-01 13:42:22 +0530

    Added a respond_error function to handle error message cases

--- core/apis/responses.py
+++ core/apis/responses.py
@@ -3,5 +3,10 @@ from flask import Response, jsonify, make_response
 class APIResponse(Response):
     @classmethod
     def respond(cls, data, status_code=200):
-        # Added the status code parameter to default to 200
+        # General success response with optional status code
         return make_response(jsonify(data=data), status_code)
+    
+    @classmethod
+    def respond_error(cls, error_type, message, status_code=400):
+        # General error response, includes error type and message
+        return make_response(jsonify(error=error_type, message=message), status_code)
